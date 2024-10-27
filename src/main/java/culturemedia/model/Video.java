73eff9d4fa1029cde11f9b0fc862e7ad538d45b4
JuanC101195 <juan.cardozor@udea.edu.co>
2024-10-27 10:48:39 -0500

    Adding video structures

---
+++ src/main/java/culturemedia/model/Video.java
@@ -0,0 +1,3 @@
+package culturoteca.model;
+
+public record Video(String code, String title, String description, Double duration) {}
---
+++ src/main/java/culturemedia/repository/VideoRepository.java
@@ -0,0 +1,11 @@
+package culturoteca.repository;
+
+import java.util.List;
+import culturoteca.model.Video;
+
+public interface VideoRepository {
+    List<Video> findAll();
+    Video save(Video save);
+    List<Video> find(String title);
+    List<Video> find(Double fromDuration, Double toDuration);
+}
